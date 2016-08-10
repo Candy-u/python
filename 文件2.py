0b7d8a4aca2277d8f4f517894eb67afc0a229e2d
Candy-u <1021522329@qq.com>
2016-08-10 16:12:28 +0800

    文件与异常

--- 文件2.py
+++ 文件2.py
@@ -7,7 +7,7 @@ os.chdir('../HeadFirstPython/chapter2') #切换目录
 
 # print(os.getcwd())
 
-data=open('sketch.txt')
+# data=open('sketch.txt')
 
 # print(data.readline(),end='')
 # print(data.readline(),end='')
@@ -64,7 +64,7 @@ data=open('sketch.txt')
 '''
 
 
-for each_line in data:
+'''for each_line in data:
     try:
         (role,line_spoken)=each_line.split(':',1)
         print(role,end='')
@@ -74,11 +74,79 @@ for each_line in data:
         pass#如果出一个运行时错误，会执行这个代码
 
 
-
-data.close()
-
+'''
 
 
 
 
+#处理缺少的文件
+
+#如果文件被删除了或者不存在，这两个解决问题的程序都会崩溃
+#第一种方法  ：增加代码检查
+'''if os.path.exists('sketch.txt'):
+    data=open('sketch.txt')
+    for each_line in data:
+        try:
+            (role,line_spoken)=each_line.split(':',1)
+            print(role,end='')
+            print('  said:',end='')
+            print(line_spoken,end='')
+        except:
+            pass
+else:
+    print('The data file is missing!')
+
+'''
+
+#第二种方法：  再做一层异常处理
+
+try:
+    data=open('sketch.txt')
+    for each_line in data:
+        try:
+            (role,line_spoken)=each_line.split(':',1)
+            print(role,end='')
+            print('  said:',end='')
+            print(line_spoken,end='')
+        except:
+            pass
+except:
+    print('The data file is missing!')
+
+
+
+#特定指定异常
+
+#如果你的异常处理代码设计为处理一种特定类型的错误，一定要在except代码行上指定错误类型，这样一来，就可以把一般化的异常处理代码转变为具有特定性。
+
+
+# except ValueError
+# except IOError
+
+'''
+open() 打开一个磁盘文件
+
+readline()读取一行数据
+
+seek() 可以将文件退回到起始位置
+
+close() 关闭文件
+
+split() 将一个字体串分为一个子串列表
+
+数据不符合期望的格式时会出现ValueError
+
+数据无法正常访问时会出现IOError
+
+find() 在一个字符串中查找特定的字串
+
+not 关键字将一个条件取反
+
+help()  bif 允许你在IDLE shell中访问Python的文档
+
+try/except 语句提供了一个异常机制，从而保护可能导致运行时错误的某些代码行。
+
+pass 空语句
+
+'''
 #
